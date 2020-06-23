bf14600b36d470df7ca5a4a87b6f4aefe1ae8fd2
Sarvamangala <sarutanush@gmail.com>
2020-06-23 18:32:54 -0500

    Demo is changed

--- Demo.java
+++ Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 		// TODO Auto-generated method stub
 		for(int i=0;i<5;i++)
 			System.out.println("Loop count="+i);
+		System.out.println("Loop count ends");
+		
 	}
 
 }
---
+++ Mavendemo/Mavendemo/App.java
@@ -0,0 +1,13 @@
+package Mavendemo.Mavendemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
